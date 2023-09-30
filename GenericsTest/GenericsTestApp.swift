5714055726ddfc09efdeb6c9592660c1e9f2189d
sasasan03 <sako.hiro62@gmail.com>
2023-09-30 16:47:10 +0900

    first Commit

---
+++ GenericsTest/GenericsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenericsTestApp.swift
+//  GenericsTest
+//
+//  Created by sako0602 on 2023/09/30.
+//
+
+import SwiftUI
+
+@main
+struct GenericsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
