c8347e36c58d8d739fcaffe9653058f7f6d1b64a
sasasan03 <sako.hiro62@gmail.com>
2023-10-04 19:10:45 +0900

    [add]: 新しいサンプルコード追加

--- GenericsTest/ContentView.swift
+++ GenericsTest/ContentView.swift
@@ -7,15 +7,89 @@
 
 import SwiftUI
 
+enum Mode {
+    case str
+    case int
+}
+
+struct Item<Elemet> {
+    var items:[Elemet] = []
+    
+    mutating func push(_ item: Elemet){
+        self.items.append(item)
+    }
+    mutating func pop(){
+        self.items.removeLast()
+    }
+}
+
 struct ContentView: View {
+    
+    @State private var modo: Mode = .str
+    @State private var textArray = Item<String>()
+    @State private var numArray = Item<Int>()
+    @State private var text = ""
+    @State private var numText = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack{
+            TabView(selection: $modo){
+                    VStack {
+                        TextField("文字", text:$text)
+                            .textFieldStyle(.roundedBorder)
+                            .padding()
+                        List(textArray.items, id: \.self ){ item in
+                            Text(item)
+                        }
+                    }.tabItem {
+                        Image(systemName: "circle.fill")
+                    }
+                    .tag(Mode.str)
+                    
+                    
+                    VStack {
+                        TextField("数", text: $numText)
+                            .textFieldStyle(.roundedBorder)
+                            .padding()
+                        List(numArray.items, id: \.self ){ item in
+                            Text("\(item)")
+                        }
+                    }
+                    .tabItem {
+                        Image(systemName: "square.fill")
+                    }
+                    .tag(Mode.int)
+            }
+            
+            .navigationTitle("items")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing){
+                    Button {
+                        if modo == Mode.int {
+                            let num = Int(numText) ?? 0
+                            numArray.push(num)
+                        } else {
+                            textArray.push(text)
+                        }
+                    } label: {
+                        Image(systemName: "tray.and.arrow.down.fill")
+                    }
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading){
+                    Button {
+                        if modo == Mode.int {
+                            numArray.pop()
+                        } else {
+                            textArray.pop()
+                        }
+                    } label: {
+                        Image(systemName: "party.popper.fill")
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
